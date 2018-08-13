0599d5027b61907426767c29eff67a60a22c4bea
Marc Majcher <majcher@gmail.com>
2018-08-13 16:10:40 -0500

    css

--- vue-app.js
+++ vue-app.js
@@ -12,8 +12,15 @@
 
     Vue.component('mb-post', {
         props: ['post'],
-        template: '<li><b>{{post.title}}:</b> <p>{{post.body}}</p></li>'
-    });
+        template: `
+            <li class="mb-post">
+                <div class="mb-post-title">{{post.title}}</div>
+                <div class="mb-post-info">By {{post.author}} ({{post.votes}})</div>
+                <img class="mb-post-thumb" :src="post.image">
+                <div class="mb-post-body">{{post.body}}</div>
+            </li>
+        `
+        });
 
     const mbApp = new Vue({ // eslint-disable-line no-unused-vars
         el: '#mb-app',
